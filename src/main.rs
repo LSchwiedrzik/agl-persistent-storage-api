b24a8bb22675c0cab4f4560e76f43b4a7bcaf121
Marvin Erler <marvin.erler@d-fine.com>
2024-04-27 14:17:48 +0200

    RocksDB Test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use rocksdb::{DB, Options};
+
 fn main() {
+    // NB: db is automatically closed at end of lifetime
+    let path = "testpath";
+    {
+        let db = DB::open_default(path).unwrap();
+        db.put(b"a", b"1").unwrap();
+        db.put(b"b", b"2").unwrap();
+        db.put(b"c", b"3").unwrap();
+        match db.get(b"a") {
+            Ok(Some(value)) => println!("retrieved value {}", String::from_utf8(value).unwrap()),
+            Ok(None) => println!("value not found"),
+            Err(e) => println!("operational problem encountered: {}", e),
+        }
+        db.delete(b"a").unwrap();
+        match db.get(b"a") {
+            Ok(Some(value)) => println!("retrieved value {}", String::from_utf8(value).unwrap()),
+            Ok(None) => println!("value not found"),
+            Err(e) => println!("operational problem encountered: {}", e),
+        }
+    }
+    let _ = DB::destroy(&Options::default(), path);
     println!("Hello, world!");
 }
